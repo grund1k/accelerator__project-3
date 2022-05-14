6df2a346077539736ccfbb53f6c232909d7ca234
grund1k <dfyz8607@gmail.com>
2022-05-14 21:06:04 +0300

    Сделал блок Тренеры

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,2 +1,31 @@
 // Swiper 7.4.1
-// import './vendor/swiper';
+import './vendor/swiper';
+
+const swiper = new Swiper('.trainers-list', {
+  slidesPerView: 4,
+  spaceBetween: 41,
+  slidesPerGroup: 4,
+  loop: false,
+  loopFillGroupWithBlank: false,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      slidesPerGroup: 1,
+    },
+    767: {
+      slidesPerView: 2,
+      spaceBetween: 31,
+      slidesPerGroup: 2,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 41,
+      slidesPerGroup: 4,
+    },
+  },
+});
